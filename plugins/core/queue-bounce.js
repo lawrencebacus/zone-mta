8bb9d12e9456488fe5817a2eb7415bedbbd9f6c3
renz <renz@snapzio.com>
2018-03-02 12:42:28 +0800

    remove console logs

--- plugins/core/queue-bounce.js
+++ plugins/core/queue-bounce.js
@@ -1,23 +1,15 @@
 'use strict';
 
 const amqp = require('amqplib');
-const fetch = require('nodemailer/lib/fetch')
 
 module.exports.title = 'HTTP Bounce Notification';
 module.exports.init = function(app, done) {
     app.addHook('queue:bounce', (bounce, maildrop, next) => {
-        // let retries = 0;
         (async function(amqp){
-          const amqpserver = fetch(app.config.amqpServer);
-
-          console.log('AMQPSERVER', 'Connect');
+          const amqpserver = app.config.amqpServer;
           const conn = await amqp.connect(amqpserver);
-
-          console.log('AMQPSERVER', 'Create Channel');
           const ch = await conn.createChannel();
-
-          const queue = fetch(app.config.queue);
-
+          const queue = app.config.queue;
           const body = {
               id: bounce.id,
               to: bounce.to,
@@ -28,16 +20,12 @@ module.exports.init = function(app, done) {
               response: bounce.response
           };
 
-          console.log('AMQPSERVER', 'Assert');
           await ch.assertQueue(queue, { durable: true });
-          console.log('AMQPSERVER', 'Send');
           await ch.sendToQueue(
             queue,
             Buffer.from(JSON.stringify(body)),
             { persistent: true }
           );
-
-          console.log('AMQPSERVER', 'Done');
         })(amqp);
     });
 
